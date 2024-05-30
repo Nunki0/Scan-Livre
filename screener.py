a27b23a764ee9d20c0152f6fb8fc74012accb629
Nunki0 <127026627+Nunki0@users.noreply.github.com>
2024-05-29 20:04:39 -0400

    1er commit

---
+++ screener.py
@@ -0,0 +1,35 @@
+import os
+import time
+import pyautogui
+
+NbPages = 10
+Décompte = 3
+
+for i in range(Décompte):
+    print("Début dans " + str(Décompte-i) + " secondes")
+    time.sleep(1)
+
+
+
+# Chemin du dossier pour sauvegarder les captures d'écran
+dossier_sauvegarde = "C:\\Users\\CTM-010676\\Documents\\Livres"
+
+# S'assurer que le dossier de sauvegarde existe, sinon le créer
+if not os.path.exists(dossier_sauvegarde):
+    os.makedirs(dossier_sauvegarde)
+
+for i in range(NbPages):
+    # Prendre une capture d'écran
+    screenshot = pyautogui.screenshot()
+
+    # Chemin complet pour sauvegarder la capture dans le dossier spécifié
+    chemin_capture = os.path.join(dossier_sauvegarde, f"capture_{i}.png")
+    screenshot.save(chemin_capture)
+
+    #Changement de page avec un clic ou la flèche droite
+    pyautogui.press('right')
+    #pyautogui.click()
+    
+    time.sleep(1.5)
+
+print("Terminé. Captures d'écran enregistrées dans", dossier_sauvegarde)
